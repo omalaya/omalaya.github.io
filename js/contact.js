ddb64301952a17943af9df1444dececc2c73249a
sidkoalex <sidkoo@outlook.com>
2017-02-08 14:27:26 +0200

    Fix contact form

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,20 @@
 $(function () {
     var $sendButton = $(".contact-form .submit")
-    var $inputName = $("input#name")
-    var $inputContact = $("input#contact")
-    var $inputMessage = $("input#message")
+    var $inputName = $("#name")
+    var $inputContact = $("#contact")
+    var $inputMessage = $("#message")
     var $sendStatus = $("#send-status");
 
-    var sendCallback = function () {
-        $sendStatus.text("✔ Відправлено.")
+    var sendCallback = function (data) {
+        $sendStatus.text("✔ Відправлено")
         $sendStatus.fadeIn()
         $sendButton.removeClass("submit-loading")
         $sendStatus.delay(3000).fadeOut()
-    }
+        $sendButton.find("span").show()
+
+        console.log("Success. Return:")
+        console.log(data)
+    };
 
     $sendButton.click(function () {
         var data = {
@@ -24,6 +28,7 @@ $(function () {
 
         sendMail(data, sendCallback)
 
+        $(this).find("span").hide()
         $(this).addClass("submit-loading")
 
         return false
